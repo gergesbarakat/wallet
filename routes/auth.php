8cba8bb13f30cf00681987bff75ded2fa3fd6e2d
gergesbarakat <girgisbarakat70@gmail.com>
2024-12-19 13:25:13 +0200

    safd

--- routes/auth.php
+++ routes/auth.php
@@ -31,16 +31,16 @@ Route::prefix('user')->name('user.')->group(function () {
 
         Route::get('/', function () {return view('auth.login');});
 
+        Route::get('login', [AuthenticatedSessionController::class, 'create'])
+            ->name('login');
+
+        Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
         Route::get('register', [RegisteredUserController::class, 'create'])
             ->name('register');
 
         Route::post('register', [RegisteredUserController::class, 'store']);
 
-        Route::get('login', [AuthenticatedSessionController::class, 'create'])
-            ->name('login');
-
-        Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
         Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
             ->name('password.request');
